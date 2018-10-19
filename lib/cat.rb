a71964372a2df39797463058ab0e25eed8d4d023
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-19 20:04:38 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,7 @@
 class Cat
   # code goes here
-  attr_accessor :name
-  def initialize(name="")
-    self.name = name
+  attr_accessor :name, :mood
+  def initialize(mood="")
+    self.mood = mood
   end
 end
