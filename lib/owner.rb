ef885d3fec23833f6bb89dbece85b19d20d93d5a
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-19 20:55:34 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -20,23 +20,23 @@ class Owner
   # def buy_fish(name="")
   #   self.pets[:fishes] << Fish.new(name)
   # end
-  # 
+  #
   # def buy_cat(name="")
   #   self.pets[:cat] << Cat.new(name)
   # end
-  # 
+  #
   # def buy_dog(name="")
   #   self.petss[:dog] << Dog.new(name)
   # end
-  # 
+  #
   # def walk_dogs
   #   self.pets[:dog].collect{|d| d.mood = "happy"}
   # end
-  # 
+  #
   # def play_with_cats
   #   self.pets[:cat].collect{|c| c.mood = "happy"}
   # end
-  # 
+  #
   # def feed_fish
   #   self.pets[:fish].collect{|f| f.mood = "happy"}
   # end
