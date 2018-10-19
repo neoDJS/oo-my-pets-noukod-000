8b5070a93d0baeba84a35a46e64f092fe4ee27b0
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-19 21:21:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,6 +3,7 @@ class Owner
   @@all = []
   attr_accessor :name
   attr_reader :species
+  attr_writer :pets
   def initialize(species="")
     @species = species
     @pets = {:fishes => [], :dogs => [], :cats => []}
