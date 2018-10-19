0f09762b1680fdc114b7aa354eb22e6b69c36bda
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-19 20:57:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -17,29 +17,29 @@ class Owner
     @pets.freeze
   end
 
-  # def buy_fish(name="")
-  #   self.pets[:fishes] << Fish.new(name)
-  # end
-  #
-  # def buy_cat(name="")
-  #   self.pets[:cat] << Cat.new(name)
-  # end
-  #
-  # def buy_dog(name="")
-  #   self.petss[:dog] << Dog.new(name)
-  # end
-  #
-  # def walk_dogs
-  #   self.pets[:dog].collect{|d| d.mood = "happy"}
-  # end
-  #
-  # def play_with_cats
-  #   self.pets[:cat].collect{|c| c.mood = "happy"}
-  # end
-  #
-  # def feed_fish
-  #   self.pets[:fish].collect{|f| f.mood = "happy"}
-  # end
+  def buy_fish(name="")
+    @pets[:fishes] << Fish.new(name)
+  end
+  
+  def buy_cat(name="")
+    @pets[:cat] << Cat.new(name)
+  end
+  
+  def buy_dog(name="")
+    @petss[:dog] << Dog.new(name)
+  end
+  
+  def walk_dogs
+    @pets[:dog].collect{|d| d.mood = "happy"}
+  end
+  
+  def play_with_cats
+    @pets[:cat].collect{|c| c.mood = "happy"}
+  end
+  
+  def feed_fish
+    @pets[:fish].collect{|f| f.mood = "happy"}
+  end
 
   def sell_pets
   end
